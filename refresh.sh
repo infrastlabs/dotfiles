37b4690d563d269f8bbe3dd2ea03a69e77069446
sam#gemmi-win10 <sam@china.gz>
2024-06-24 19:07:58 +0800

    refresh.sh: bash /etc/skel/.fluxbox/fluxbox.sh;
    refresh mkdir -p /usr/share/backgrounds/xfce/; \
    
    echo "export $one" >> /.env2

--- refresh.sh
+++ refresh.sh
@@ -25,10 +25,17 @@ cd $cur
   mkdir -p /usr/share/plank/themes/Default
   \cp -a clear3d.theme /usr/share/plank/themes/Default/dock.theme
 
+  # env
+  : > /.env2
+  cat env |egrep -v "^#|^$" |while read one; do
+    echo "export $one" >> /.env2
+  done
+
 # bg,icon,theme: wget -qO
 $RUN cd /etc/skel/.config/plank/dock1/launchers && rm -f ristretto* geany* flameshot*; \
   \
   export assets_repo=https://gitee.com/infrastlabs/docker-headless; \
+  mkdir -p /usr/share/backgrounds/xfce/; \
   curl -k -fsSL -o /usr/share/backgrounds/xfce/xfce-teal.png $assets_repo/raw/dev/_doc/assets/bg/bg-debian-liteblue.png; \
   curl -k -fsSL -o /usr/share/backgrounds/xfce/xfce-pure-blue.jpg $assets_repo/raw/dev/_doc/assets/bg/pure-blue.jpg; 
   # \
@@ -39,6 +46,9 @@ $RUN cd /etc/skel/.config/plank/dock1/launchers && rm -f ristretto* geany* flame
   sed -i "s^value=\"Mint-Y\"^value=\"Papirus-Bunsen-bluegrey\"^g" /etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml; \
   sed -i "s^xfce-teal.png^xfce-pure-blue.jpg^g" /etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml;
 
+
+# cp from Dockerfile.xx
+bash /etc/skel/.fluxbox/fluxbox.sh; 
 
 # skel: copy
 \cp -a /etc/skel/. /home/headless/; chown -R headless:headless /home/headless;
